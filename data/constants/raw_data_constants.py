669104ea9be47d15f24bf65f4ffc518189b16fdf
trsaints <thiago.dev2k3@gmail.com>
2025-09-21 13:22:12 -0300

    feat(constants): provide column constants for raw ONGs dataset

---
+++ data/constants/raw_data_constants.py
@@ -0,0 +1,23 @@
+class OngsDatasetCols:
+    """
+    Constants for column names in the raw ONG dataset.
+    """
+    CNPJ = "cnpj"
+    RAZAO_SOCIAL = "tx_razao_social_osc"
+    NOME_FANTASIA = "tx_nome_fantasia_osc"
+    NATUREZA_JURIDICA = "cd_natureza_juridica_osc"
+    DATA_FUNDACAO = "dt_fundacao_osc"
+    SITUACAO_CADASTRAL = "situacao_cadastral"
+
+    ENDERECO = "tx_endereco_completo"
+    MUNICIPIO_ID = "cd_municipio"
+    MUNICIPIO_NOME = "municipio_nome"
+    UF = "UF_Sigla"
+
+    CNAE = "cnae"
+
+    ASSISTENCIA_SOCIAL = "Area_Assistencia_social"
+    EDUCACAO = "Area_Educacao_e_pesquisa"
+    SAUDE = "Area_Saude"
+    CULTURA = "Area_Cultura_e_recreacao"
+    RELIGIAO = "Area_Religiao"
