b861c0a16ef99e0204da78a1da6f7afbb9070b88
trsaints <thiago.dev2k3@gmail.com>
2025-09-22 21:41:09 -0300

    update(etl): provide proper data treatment depending on file extension

--- data/processing/data_parser.py
+++ data/processing/data_parser.py
@@ -1,10 +1,11 @@
 from typing import Optional
 import pandas as pd
+import os
 
 from data.constants.dataset_constants import DATASET_DIR
 
 
-def to_utf8(dataset_name: str) -> Optional[str]:
+def to_utf8(dataset_filename: str) -> Optional[str]:
     """
     Converts a dataset to UTF-8 encoding and saves it with a new suffix.
     This method assumes the input dataset is encoded in 'cp860', which is the encoding for legacy systems for the Portuguese language.
@@ -19,23 +20,48 @@ def to_utf8(dataset_name: str) -> Optional[str]:
         INPUT_ENCODING = "cp860"
         OUTPUT_ENCODING = "utf-8"
 
-        main_df = pd.read_csv(DATASET_DIR + dataset_name,
-                              encoding=INPUT_ENCODING, sep=";",
-                              on_bad_lines="skip")
-        output_dir = output_name(dataset_name)
+        full_path = DATASET_DIR + dataset_filename
+        path_exists = os.path.exists(full_path)
 
-        main_df.to_csv(output_dir,
+        if not path_exists:
+            print(f"File {full_path} does not exist.")
+
+            return None
+
+        filename, _ = os.path.splitext(dataset_filename)
+
+        print(
+            f"Dataset '{filename}' found in {full_path}. Converting to UTF-8...")
+
+        main_df: pd.DataFrame
+
+        if dataset_filename.endswith(".xlsx"):
+            main_df = pd.read_excel(full_path,
+                                    engine="openpyxl")
+        elif dataset_filename.endswith(".csv"):
+            main_df = pd.read_csv(full_path,
+                                  encoding=INPUT_ENCODING, sep=";",
+                                  on_bad_lines="skip")
+        else:
+            main_df = pd.read_html(full_path,
+                                   encoding=INPUT_ENCODING)[0]
+
+        result_path = output_path(filename)
+
+        main_df.to_csv(result_path,
                        encoding=OUTPUT_ENCODING,
                        sep=";",
                        index=False)
 
-        return output_dir
+        return result_path
+
     except Exception as e:
-        print(f"Error converting {dataset_name} to UTF-8: {e}")
+        print(f"Error converting {dataset_filename} to UTF-8: {e}")
+
         return None
 
 
-def output_name(dataset_name: str) -> str:
+def output_path(dataset_filename: str) -> str:
     """
     Generates the output filename for a UTF-8 converted dataset.
 
@@ -46,4 +72,4 @@ def output_name(dataset_name: str) -> str:
         str: The output filename with a UTF-8 suffix.
     """
     OUTPUT_SUFFIX = "_utf8"
-    return DATASET_DIR + dataset_name + OUTPUT_SUFFIX + ".csv"
+    return DATASET_DIR + dataset_filename + OUTPUT_SUFFIX + ".csv"
