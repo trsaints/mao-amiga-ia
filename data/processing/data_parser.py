3ca1b52f29bb002a0304299ad3a87f81804162ec
trsaints <thiago.dev2k3@gmail.com>
2025-09-22 22:15:21 -0300

    update: revert previous change

--- data/processing/data_parser.py
+++ data/processing/data_parser.py
@@ -71,7 +71,7 @@ def output_path(dataset_filename: str) -> str:
     Returns:
         str: The output filename with a UTF-8 suffix.
     """
-    OUTPUT_SUFFIX = "_result"
+    OUTPUT_SUFFIX = "_utf8"
     return DATASET_DIR + dataset_filename + OUTPUT_SUFFIX + ".csv"
 
 
