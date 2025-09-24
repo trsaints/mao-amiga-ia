fcbdca01b6599ea232d5429e1433bfc843f8f5a5
trsaints <thiago.dev2k3@gmail.com>
2025-09-24 01:06:53 -0300

    update(projects): ensure irrelevant data may be discarded from final report

--- data/processing/projects.py
+++ data/processing/projects.py
@@ -64,7 +64,11 @@ def projects_dataset(source: pd.DataFrame) -> pd.DataFrame:
 
     renamed = main_columns.rename(columns=columns_map)
     result = (pd.concat([renamed.reset_index(drop=True), status_df], axis=1)
-              .drop_duplicates(["ID Projeto"]))
+              .drop_duplicates(["ID Projeto"])
+              .dropna(how="any", subset=["Data de Início"])
+              .dropna(how="all", subset=["Total de Beneficiários",
+                                         "Valor Captado (R$)",
+                                         "Valor Total (R$)"]))
 
     return result
 
